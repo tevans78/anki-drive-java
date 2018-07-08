a57127cd22b48dd3b9926e5ec56e25e8b8286472
Tom Evans <tevans@uk.ibm.com>
2018-07-08 20:47:12 +0100

    Connection Servlet

--- AnkiTestWebApp/src/main/java/io/openliberty/anki/cdi/Anki.java
+++ AnkiTestWebApp/src/main/java/io/openliberty/anki/cdi/Anki.java
@@ -1,5 +1,6 @@
 package io.openliberty.anki.cdi;
 
+import java.util.Collection;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -7,54 +8,76 @@ import javax.enterprise.context.ApplicationScoped;
 import de.adesso.anki.MessageListener;
 import de.adesso.anki.Vehicle;
 import de.adesso.anki.messages.LightsPatternMessage;
-import de.adesso.anki.messages.Message;
-import de.adesso.anki.messages.SetSpeedMessage;
 import de.adesso.anki.messages.LightsPatternMessage.LightChannel;
 import de.adesso.anki.messages.LightsPatternMessage.LightConfig;
 import de.adesso.anki.messages.LightsPatternMessage.LightEffect;
+import de.adesso.anki.messages.Message;
+import de.adesso.anki.messages.MessageMap;
+import de.adesso.anki.messages.SetSpeedMessage;
 import de.adesso.anki.tinyb.AnkiConnectorTinyB;
-import de.adesso.anki.tinyb.TinyBTest;
 
 @ApplicationScoped
 public class Anki implements MessageListener<Message> {
 
 	private AnkiConnectorTinyB anki = new AnkiConnectorTinyB();
-	
+
 	public void test() throws InterruptedException {
-        
-        try {
-            List<Vehicle> vehicles = anki.findVehicles();
-            for (Vehicle v : vehicles) {
-                if (v.getAddress().equals("EE:09:1D:49:59:FC")) {
-                    v.connect();
-                    v.addMessageListener(Message.class, this);
 
-                    for (int i = 0; i < 1; i++) {
-                        LightsPatternMessage lights = new LightsPatternMessage();
-                        lights.add(new LightConfig(LightChannel.FRONT_RED, LightEffect.STROBE, 0, 1, 2));
-                        v.sendMessage(lights);
+		try {
+			List<Vehicle> vehicles = anki.findVehicles();
+			for (Vehicle v : vehicles) {
+				if (v.getAddress().equals("EE:09:1D:49:59:FC")) {
+					v.connect();
+					v.addMessageListener(Message.class, this);
 
-                        SetSpeedMessage speed = new SetSpeedMessage(200, 1000);
-                        v.sendMessage(speed);
+					for (int i = 0; i < 1; i++) {
+						LightsPatternMessage lights = new LightsPatternMessage();
+						lights.add(new LightConfig(LightChannel.FRONT_RED, LightEffect.STROBE, 0, 1, 2));
+						v.sendMessage(lights);
 
-                        Thread.sleep(5000);
+						SetSpeedMessage speed = new SetSpeedMessage(200, 1000);
+						v.sendMessage(speed);
 
-                        speed = new SetSpeedMessage(0, 1000);
-                        v.sendMessage(speed);
-                    }
+						Thread.sleep(5000);
 
-                    v.disconnect();
-                }
-            }
+						speed = new SetSpeedMessage(0, 1000);
+						v.sendMessage(speed);
+					}
 
-        } finally {
-            anki.close();
-        }
-    }
+					v.disconnect();
+				}
+			}
 
-    @Override
-    public void messageReceived(Message message) {
-        System.out.println("Message! " + message);
-    }
+		} finally {
+			anki.close();
+		}
+	}
+
+	@Override
+	public void messageReceived(Message message) {
+		System.out.println("Message! " + message);
+	}
+
+	public List<Vehicle> listVehicles() {
+		return anki.findVehicles();
+	}
+
+	public void connect(String address) {
+		for (Vehicle v : anki.findVehicles()) {
+			if (v.getAddress().equals(address)) {
+				v.connect();
+				break;
+			}
+		}
+	}
 	
+	public void disconnect(String address) {
+		for (Vehicle v : anki.findVehicles()) {
+			if (v.getAddress().equals(address)) {
+				v.disconnect();
+				break;
+			}
+		}
+	}
+
 }
--- AnkiTestWebApp/src/main/java/io/openliberty/anki/web/AnkiTest.java
+++ AnkiTestWebApp/src/main/java/io/openliberty/anki/web/AnkiTest.java
@@ -1,6 +1,9 @@
 package io.openliberty.anki.web;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -9,6 +12,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import de.adesso.anki.Vehicle;
+import de.adesso.anki.messages.Message;
+import de.adesso.anki.messages.MessageMap;
 import io.openliberty.anki.cdi.Anki;
 
 /**
@@ -17,27 +23,68 @@ import io.openliberty.anki.cdi.Anki;
 @WebServlet("/AnkiTest")
 public class AnkiTest extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 	@Inject
 	Anki anki;
-	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AnkiTest() {
-        super();
-        System.out.println("LIB: "+System.getProperty("java.library.path"));
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public AnkiTest() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		try {
-			anki.test();
-		} catch (InterruptedException e) {
+			Map<Integer, Class<? extends Message>> messageClasses = MessageMap.MESSAGES;
+			List<Vehicle> vehicles = anki.listVehicles();
+
+			StringBuilder builder = new StringBuilder();
+			builder.append("<html><head><title>Anki</title></head><body>");
+			builder.append("<form action=\"/AnkiTest\" method=\"post\">");
+			for (Vehicle v : vehicles) {
+				builder.append("<input type=\"radio\" name=\"address\" value=\"");
+				builder.append(v.getAddress());
+				builder.append("\"> ");
+				builder.append(v.getAddress());
+				builder.append("<br>");
+			}
+			builder.append("<input type=\"submit\" name=\"action\" value=\"Connect\">");
+			builder.append("<input type=\"submit\" name=\"action\" value=\"Disconnect\">");
+			
+			for (Map.Entry<Integer, Class<? extends Message>> clazz : messageClasses.entrySet()) {
+				builder.append("<input type=\"radio\" name=\"messageType\" value=\"");
+				builder.append(clazz.getKey());
+				builder.append("\"> ");
+				builder.append(clazz.getValue().getSimpleName());
+				builder.append("<br>");
+			}
+			
+			builder.append("</form>");
+			builder.append("</body></html>");
+			
+			response.getWriter().println(builder.toString());
+
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String address = (String) request.getParameter("address");
+		String action = request.getParameter("action");
+		if(action.equals("Connect")) {
+			anki.connect(address);
+		}
+		else {
+			anki.disconnect(address);
 		}
 	}
 }
